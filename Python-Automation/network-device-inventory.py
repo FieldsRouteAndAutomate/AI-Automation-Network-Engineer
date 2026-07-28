d0bcd1a33b7082523129269e372fc50faf3f6804
FieldsRouteAndAutomate <fields743@gmail.com>
2026-07-28 00:45:26 -0400

    Add future plans for Cisco device connectivity
    Plan to expand script functionality for Cisco device data collection.

--- Python-Automation/network-device-inventory.py
+++ Python-Automation/network-device-inventory.py
@@ -6,3 +6,10 @@ devices = [
 
 for device in devices:
     print(device)
+
+Later I'll expand this script to connect to Cisco devices and collect:
+
+Hostname
+IP Address
+IOS Version
+Serial Number
